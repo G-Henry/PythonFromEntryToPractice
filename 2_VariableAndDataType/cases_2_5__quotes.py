9092032272674bd83dd973ed51868083c3f34b97
Henry <henry.zhang@foxconn.com>
2019-09-16 08:37:07 +0800

    V2 Merge working in Office

---
+++ 2_VariableAndDataType/cases_2_5__quotes.py
@@ -0,0 +1,3 @@
+celebrity_name = "Albert Einstein"
+message = r'''{0} once said, "A person who never made a mistake never tried anything new."'''
+print(message.format(celebrity_name))
---
+++ 2_VariableAndDataType/hello_word.py
@@ -0,0 +1,6 @@
+message = "Hello Python world!"
+print(message)
+
+favorite_language = '\n\n\n\npython ssss basd \n\n\t\n'
+favorite_language = favorite_language.lstrip()
+print(favorite_language)
